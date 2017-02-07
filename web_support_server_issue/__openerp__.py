59a7c027aaf9b915464fccde9b4b87a2fddf205e
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-02-07 14:48:05 -0300

    IMP usability

--- web_support_server_issue/__openerp__.py
+++ web_support_server_issue/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Web Support Issue - Server',
-    'version': '9.0.1.1.0',
+    'version': '9.0.1.2.0',
     'category': 'Support',
     'sequence': 14,
     'summary': '',
