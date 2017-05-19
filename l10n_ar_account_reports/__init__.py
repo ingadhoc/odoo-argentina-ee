c4222f77695279ae9f8ecf172337d7afddcfa586
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-05-19 14:23:44 -0300

    MOVE l10n_ar_account_reports to other repo

--- l10n_ar_account_reports/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
-# directory
-##############################################################################
-from . import models
-from . import reports
-from . import wizards
-from . import res_config
