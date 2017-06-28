0fcb1b93df676e3551a517193f4964087cb493e9
Juan José Scarafía <jjs@adhoc.com.ar>
2017-06-28 11:25:28 -0300

    Revert "DEL sale_contract_lines_sequence" (#14)
    This reverts commit 38bcc07765d723f03ee7ca231f1d86e760c5cd37.

---
+++ sale_contract_lines_sequence/sale_subscription_line.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from openerp import models, fields
+
+
+class SaleSubscriptionLine(models.Model):
+    _inherit = 'sale.subscription.line'
+    _order = 'sequence'
+
+    sequence = fields.Integer(
+        'Sequence',
+        required=True,
+        default=10,
+        help="The sequence field is used to order lines"
+    )
