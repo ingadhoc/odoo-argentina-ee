8690b66010343f82bf2d6120a5aad5adaf260885
Katherine Zaoral <zaoral@users.noreply.github.com>
2018-03-13 10:13:28 -0300

    [REM] web_support_server deprecated for version 11.0 (#30)

--- web_support_server/models/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import res_users
-from . import res_partner
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
