0ea4c28b8e5afc0f5952738c4ba8023549c75581
Katherine Zaoral <zaoral@users.noreply.github.com>
2018-03-13 10:04:12 -0300

    [REM] Deprecated infrastructure_contract module. (#27)
    This one was used for 8.0 customer instances. This will not be supported
    anymore.

--- infrastructure_contract/models/__init__.py
+++
@@ -1,9 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import database
-from . import sale_subscription
-from . import odoo_version
-from . import product_template
-from . import sale_subscription_line
