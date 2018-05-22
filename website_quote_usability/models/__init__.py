1711c55f05222b8b0e266659991d9dfd08a79993
Katherine Zaoral <zaoral@users.noreply.github.com>
2018-05-22 16:58:07 -0300

    [REL] website_quote_usability: Deprecated module version 11.0 (#53)

--- website_quote_usability/models/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import sale_quote_template
-from . import sale_order
-from . import sale_subscription
