039b14687f6356d9007f9eddaeb532af1d534b0e
Katherine Zaoral <zaoral@users.noreply.github.com>
2018-09-12 18:39:55 -0300

    [REL] l10n_ar_currency_update: Migration to version 11.0 (#86)
    * [REL] l10n_ar_currency_update: Migration to version 11.0
    
    - move module from odoo-argentina repository to enterprise-extensions.
    - add odoo-argentina repository to oca_dependencies to make the module works.
    - fix: remove execution permissions from init file.
    - clean up to match new readme guideline. update readme with a simple
      description (it does not have any description before). Also add information
      for configuration.
    - remove deprecated description key from manifest.
    - change module version
    - set module installable
    - replace old oca module currency_rate_live with the new one for odoo
      enterprise currency_rate_update
    - delete not nedeed currency_rate_update models.
    - leave to configure the rate_perc and rate_surcharge in the
      res.confi.settings in the same section as the currency provider
      configurations are.
    - adapt res_company model to work as it does in currency_rate_live odoo
      module.
    - use hooks for post init config currency provider.
    - clean up manifest
    
    * [FIX] l10n_ar_currency_update: unique per day constraint for rates
    
    Adapt code to proper popup the unique rate per day constraint, this already
    exist but our code logic was capturing the exception and showing it wrong.
    
    * [IMP] l10n_ar_currency_update: rate perc/surcharge visible now when technical
    features active.
    
    * [FIX] l10n_ar_currency_update: pylint errors

---
+++ l10n_ar_currency_update/models/res_company.py
@@ -0,0 +1,130 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class ResCompany(models.Model):
+
+    _inherit = 'res.company'
+
+    currency_provider = fields.Selection(
+        selection_add=[('afip', 'AFIP Web Service (Argentina)')],
+        default='afip',
+    )
+    rate_perc = fields.Float(
+        digits=(16, 4),
+    )
+    rate_surcharge = fields.Float(
+        digits=(16, 4),
+    )
+
+    @api.model
+    def create(self, values):
+        """ Overwrite to include new currency provider """
+        if values.get('country_id') and 'currency_provider' not in values:
+            country = self.env['res.country'].browse(values['country_id'])
+            if country.code.upper() == 'AR':
+                values['currency_provider'] = 'afip'
+        return super(ResCompany, self).create(values)
+
+    @api.model
+    def set_special_defaults_on_install(self):
+        """ Overwrite to include new currency provider """
+        super(ResCompany, self).set_special_defaults_on_install()
+        ar_companies = self.search([]).filtered(
+            lambda company: company.country_id.code == 'AR')
+        if ar_companies:
+            ar_companies.update({
+                'currency_provider': 'afip',
+                'currency_interval_unit': 'daily',
+            })
+            _logger.log(
+                25, "Currency Provider configured as AFIP for next"
+                " companies: %s", ', '.join(ar_companies.mapped('name')))
+
+    @api.multi
+    def update_currency_rates(self):
+        """ Overwrite to include new currency provider """
+        res = super(ResCompany, self).update_currency_rates()
+        afip_companies = self.search([]).filtered(
+            lambda company: company.currency_provider == 'afip')
+        for company in afip_companies:
+            _logger.log(25, "Connecting to AFIP to update the currency rates"
+                        " for %s", company.name)
+            res = company._update_currency_afip()
+            if not res:
+                _logger.warning(_(
+                    'Unable to connect to the online exchange rate platform'
+                    ' %s. The web service may be temporary down.') %
+                    company.currency_provider)
+                continue
+            company.last_currency_sync_date = fields.Date.today()
+        return res
+
+    @api.multi
+    def _update_currency_afip(self):
+        """ This method is used to update the currency rates using AFIP provider
+        provider. Rates are given against AR.
+        """
+        self.ensure_one()
+        _logger.log(
+            25,
+            'Starting to refresh currencies with provider %s (company: %s)',
+            self.currency_provider, self.name)
+        rate_obj = self.env['res.currency.rate']
+
+        # Check proper main currency configuration
+        main_currency = self.currency_id
+        if not main_currency:
+            raise UserError(_('There is no main currency defined!'))
+        if main_currency.name != 'ARS':
+            raise UserError(_('For AFIP WS base currency must be ARS!'))
+        if main_currency.rate != 1:
+            raise UserError(_('Base currency rate should be 1.00!'))
+
+        # Obtain the currencies to be updated
+        afip_supported_currency = [
+            'USD', 'EUR', 'AUD', 'CAD', 'GBP', 'JPY', 'MXN', 'UYU', 'VEF']
+        available_currencies = self.env['res.currency'].search([])
+        currency_to_update = list(
+            set(available_currencies.mapped('name')).intersection(
+                set(afip_supported_currency))
+        )
+        currency_to_update = self.env['res.currency'].search(
+            [('name', 'in', currency_to_update)])
+        rate_name = fields.Date.today()
+        for currency in currency_to_update:
+            try:
+                # Do not pass company since we need to find the one that has
+                # certificate
+                rate, msg = currency.get_pyafipws_currency_rate()
+            except Exception as exc:
+                _logger.error(repr(exc))
+
+            if rate:
+                # impr when ARS is not base currency (rate 1.0)
+                # company_rate = rate / base_currency_rate
+                rate = rate * (1.0 + (self.rate_perc or 0.0))
+                rate += self.rate_surcharge or 0.0
+                rate = 1.0 / rate
+                rate_obj.create({
+                    'currency_id': currency.id,
+                    'rate': rate,
+                    'name': rate_name,
+                    'company_id': self.id
+                })
+                _logger.info(
+                    'Updated currency %s via provider %s',
+                    currency.name, self.currency_provider)
+            else:
+                raise UserError(_(
+                    'Could not update currency %s via provider %s. %s',
+                    currency.name, self.currency_provider, msg
+                ))
+        return True
---
+++ l10n_ar_currency_update/wizards/res_config_settings.py
@@ -0,0 +1,13 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+
+    _inherit = 'res.config.settings'
+
+    rate_perc = fields.Float(related="company_id.rate_perc")
+    rate_surcharge = fields.Float(related="company_id.rate_surcharge")
