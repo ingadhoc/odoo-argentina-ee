0e3a98b47e53b8157e730a0d3b8aab9dfd7e2b50
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-04-08 15:59:51 -0300

    [REM] deprecate crm_voip_usability

--- crm_voip_usability/models/__init__.py
+++
@@ -1,8 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-
-from . import res_partner
-from . import crm_lead
-from . import crm_phonecall
