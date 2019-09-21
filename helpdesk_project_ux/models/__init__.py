9605d821c4e847a0b95cb2bc5c02a3aca78e6a5f
Lucas L. Soto <ls@adhoc.com.ar>
2019-09-21 11:30:52 -0300

    [REF] helpdesk_project_ux: rename module to helpdesk_timesheet_ux (#156)
    * [REF] helpdesk_project_ux: rename module to helpdesk_timesheet_ux
    
    * [REF] portal_helpdesk: change dependency
    
    - Change dependency from helpdesk_project_ux to helpdesk_timesheet_ux
    
    * [FIX] use icon insted of "tickets" on project kanban

--- helpdesk_project_ux/models/__init__.py
+++
@@ -1,9 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-
-from . import project_project
-from . import project_task_type
-from . import helpdesk_ticket
-from . import helpdesk_team
