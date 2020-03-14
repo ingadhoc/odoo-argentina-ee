f86902eca66af8069417f2036e49c8beb25ea020
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-03-14 14:25:14 -0300

    [FIX] l10n_ar_demo

--- l10n_ar_demo/__init__.py
+++ l10n_ar_demo/__init__.py
@@ -17,7 +17,7 @@ old_load_information_from_description_file = module.load_information_from_descri
 
 def load_information_from_description_file(module, mod_path=None):
     info = old_load_information_from_description_file(module, mod_path=mod_path)
-    if module == 'l10n_ar':
+    if module in ['l10n_ar', 'l10n_ar_edi']:
         info['demo'] = []
     return info
 
@@ -27,14 +27,15 @@ module.load_information_from_description_file = load_information_from_descriptio
 
 def _load_l10n_ar_demo_data(cr):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    _logger.info('Loading demo data from l10n_ar')
-    manifest_file = module.module_manifest(module.get_module_path('l10n_ar'))
-    f = tools.file_open(manifest_file, mode='rb')
-    try:
-        info = ast.literal_eval(tools.pycompat.to_text(f.read()))
-    finally:
-        f.close()
-    for demo_data in info.get('demo'):
-        _logger.info('Loading %s', (demo_data))
-        tools.convert_file(cr, 'l10n_ar', demo_data, {}, 'init', True, 'demo', None)
-    env.cr.commit()
+    for module_name in ['l10n_ar', 'l10n_ar_edi']:
+        _logger.info('Loading demo data from %s' % module_name)
+        manifest_file = module.module_manifest(module.get_module_path(module_name))
+        f = tools.file_open(manifest_file, mode='rb')
+        try:
+            info = ast.literal_eval(tools.pycompat.to_text(f.read()))
+        finally:
+            f.close()
+        for demo_data in info.get('demo'):
+            _logger.info('Loading %s', (demo_data))
+            tools.convert_file(cr, module_name, demo_data, {}, 'init', True, 'demo', None)
+        env.cr.commit()
