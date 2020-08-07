25bb833f00fe7534c61227ab733a818d17404b62
Katherine Zaoral <kz@adhoc.com.ar>
2020-08-07 12:54:41 -0300

    [FIX] l10n_ar_account_tax_settlement: ret iibb txt file
    the way that we were computing the column 9 was wrong.
    We were using the values of perception column 7 and we shoud use the
    values of retentions column 7 instead

--- l10n_ar_account_tax_settlement/models/account_journal.py
+++ l10n_ar_account_tax_settlement/models/account_journal.py
@@ -874,8 +874,8 @@ class AccountJournal(models.Model):
                 alicuot_line.alicuota_retencion, 6, 2, '.'))
 
             # 9 Monto retenido
-            # TODO this need to be changed when we properly compute the perception amounts and use line.balance again
-            content.append(format_amount(round_half_up(-get_line_tax_base(line) * (alicuot_line.alicuota_retencion / 100.0), 2), 12, 2, '.'))
+            # TODO this need to be changed when we properly compute the retentions amounts and use line.balance again
+            content.append(format_amount(round_half_up(payment.withholdable_base_amount * (alicuot_line.alicuota_retencion / 100.0), 2), 12, 2, '.'))
 
             # 10 Tipo de Régimen de Percepción
             # (código correspondiente según tabla definida por la jurisdicción)
