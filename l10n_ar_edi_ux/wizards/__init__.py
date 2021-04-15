71ed1bc358f1125e822aab3108d6382910f54dfd
Bruno Zanotti (ADHOC) <bz@adhoc.com.ar>
2021-04-15 09:07:58 -0300

    [ADD] l10n_ar_edi_ux: CN/DN from customer receipts
    We add new fields on the "Credit / Debit Note" wizard to support the RG 4540 when creating a debit/credit note from customer receipts.
    When posting debit/credit notes to AFIP we need to report the original document. If there is not original document related, then the user can set the PeriodoAsoc information.
    
    New fields:
     * l10n_ar_afip_asoc_period_start
     * l10n_ar_afip_asoc_period_end
     * origin_invoice_id

--- l10n_ar_edi_ux/wizards/__init__.py
+++ l10n_ar_edi_ux/wizards/__init__.py
@@ -4,3 +4,4 @@
 ##############################################################################
 from . import res_partner_update_from_padron_wizard
 from . import res_config_settings
+from . import account_payment_group_invoice_wizard
---
+++ l10n_ar_edi_ux/wizards/account_payment_group_invoice_wizard.py
@@ -0,0 +1,33 @@
+from odoo import fields, models
+
+
+class AccountPaymentGroupInvoiceWizard(models.TransientModel):
+    _inherit = "account.payment.group.invoice.wizard"
+
+    l10n_ar_afip_asoc_period_start = fields.Date(
+        'Associate Period From',
+    )
+
+    l10n_ar_afip_asoc_period_end = fields.Date(
+        'Associate Period To',
+    )
+
+    origin_invoice_id = fields.Many2one(
+        'account.move',
+    )
+
+    commercial_partner_id = fields.Many2one(
+        'res.partner',
+        related="payment_group_id.partner_id.commercial_partner_id"
+    )
+
+    def get_invoice_vals(self):
+        self.ensure_one()
+        invoice_vals = super().get_invoice_vals()
+        origin_doc = "reversed_entry_id" if invoice_vals['type'] in ['in_refund', 'out_refund'] else "debit_origin_id"
+        invoice_vals.update({
+            'l10n_ar_afip_asoc_period_start': self.l10n_ar_afip_asoc_period_start,
+            'l10n_ar_afip_asoc_period_end': self.l10n_ar_afip_asoc_period_end,
+            origin_doc: self.origin_invoice_id
+        })
+        return invoice_vals
