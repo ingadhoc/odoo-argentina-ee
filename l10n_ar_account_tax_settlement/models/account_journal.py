b1720e5ae34e6f2cdc0bcd67fef2e6ac2736b377
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-05-10 16:34:23 -0300

    [13.0] [FIX] l10n_ar_account_tax_settlement: Use the correct field.

--- l10n_ar_account_tax_settlement/models/account_journal.py
+++ l10n_ar_account_tax_settlement/models/account_journal.py
@@ -612,7 +612,7 @@ class AccountJournal(models.Model):
                 # convert to company currency if in another currency
                 if company_currency != line.move_id.currency_id:
                     taxable_amount = company_currency.round(
-                        taxable_amount * line.move_id.currency_rate)
+                        taxable_amount * line.move_id.l10n_ar_currency_rate)
 
                 # tambien lo sacamos por diferencia para no tener error (por el
                 # calculo trucado de taxable_amount por ejemplo) y
