9c09c3a0da8c28e075839ca8b2ebc071c33118ec
Katherine Zaoral <kz@adhoc.com.ar>
2021-06-22 23:39:53 -0300

    [ADD] l10n_ar_edi_ux: add padron cuits for test
    Add demo data and update the URl with CUITs that can be use in testing
    environment.

--- l10n_ar_edi_ux/__manifest__.py
+++ l10n_ar_edi_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Electronic Invoicing UX',
-    'version': "13.0.1.5.0",
+    'version': "13.0.1.6.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
@@ -21,6 +21,9 @@
         'views/res_partner_view.xml',
         'views/account_move_view.xml',
         'views/account_journal_view.xml',
+    ],
+    'demo': [
+        'demo/res_partner_demo.xml',
     ],
     'installable': True,
     'auto_install': True,
