c10e87fdc3ab91089b0c7d5a94308c0ed83cf219
raguero </SUlem@2017/>
2021-11-22 22:36:02 -0300

    [MIG] l10n_ar_account_tax_settlement: Migration to 15.0

--- l10n_ar_account_tax_settlement/__manifest__.py
+++ l10n_ar_account_tax_settlement/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Tax Settlements For Argentina',
-    'version': '13.0.1.2.0',
+    'version': "15.0.1.0.0",
     'category': 'Accounting',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
@@ -41,7 +41,7 @@
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'application': False,
 }
--- l10n_ar_account_tax_settlement/models/account_chart_template.py
+++ l10n_ar_account_tax_settlement/models/account_chart_template.py
@@ -14,7 +14,6 @@ class AccountChartTemplate(models.Model):
         res = super(
             AccountChartTemplate, self)._create_bank_journals(
             company, acc_template_ref)
-
         if company.country_id != self.env.ref('base.ar'):
             return res
 
@@ -24,13 +23,12 @@ class AccountChartTemplate(models.Model):
             """
             Helper para obtener referencia a alguna cuenta
             """
-            return acc_template_ref.get(ref('l10n_ar.%s' % ext_id).id)
+            return acc_template_ref.get(ref('l10n_ar.%s' % ext_id))
 
         # add more journals commonly used in argentina localization
-        journals = [
+        journals_data = [
             ('Liquidación de IIBB', 'IIBB', 'allow_per_line', 'iibb_sufrido',
                 ref('l10n_ar.par_iibb_pagar'),
-                get_account('base_iibb_a_pagar'),
                 get_account('base_iibb_a_pagar'),
                 ref('l10n_ar_ux.tax_tag_a_cuenta_iibb'))
         ]
@@ -41,12 +39,11 @@ class AccountChartTemplate(models.Model):
 
         # iva solo para RI
         if chart == ri_chart:
-            journals += [
+            journals_data += [
                 ('Liquidación de IVA', 'IVA', 'yes', False,
                     # ref('l10n_ar_ux_reports.'
                     #     'account_financial_report_vat_position'),
                     ref('l10n_ar.partner_afip'),
-                    get_account('ri_iva_saldo_tecnico_favor'),
                     get_account('ri_iva_saldo_a_pagar'),
                     ref('l10n_ar_ux.tax_tag_a_cuenta_iva'))]
 
@@ -60,20 +57,18 @@ class AccountChartTemplate(models.Model):
 
         # estas para mono no van, para exento y ri si
         if chart in (ri_chart, ex_chart):
-            journals += [
+            journals_data += [
                 ('Liquidación de Ganancias', 'GAN', 'yes', False,
                     # ref('l10n_ar_ux_reports.'
                     #     'account_financial_report_profits_position'),
                     ref('l10n_ar.partner_afip'),
-                    get_account('base_saldo_favor_ganancias'),
                     get_account('base_impuesto_ganancias_a_pagar'),
                     ref('l10n_ar_ux.tax_tag_a_cuenta_ganancias')),
                 # only if account_withholding_automatic installed we
                 # set sicore_aplicado for txt
                 ('Liquidación SICORE Aplicado', 'SICORE', 'allow_per_line',
-                    account_withholding_automatic and 'sicore_aplicado',
+                    account_withholding_automatic and 'sicore_aplicado' or False,
                     ref('l10n_ar.partner_afip'),
-                    get_account('ri_retencion_sicore_a_pagar'),
                     get_account('ri_retencion_sicore_a_pagar'),
                     ref('l10n_ar_ux.tag_ret_perc_sicore_aplicada')),
                 ('Liquidación IIBB Aplicado', 'IB_AP', 'allow_per_line',
@@ -81,15 +76,13 @@ class AccountChartTemplate(models.Model):
                     ref('l10n_ar.par_iibb_pagar'),
                     # TODO flatan crear estas cuentas!
                     get_account('ri_retencion_iibb_a_pagar'),
-                    get_account('ri_retencion_iibb_a_pagar'),
                     ref('l10n_ar_ux.tag_ret_perc_iibb_aplicada')),
             ]
 
         # for name, code, tax, report, partner, credit_id, debit_id, tag \
-        for name, code, type, tax, partner, credit_id, debit_id, tag \
-                in journals:
-            if not credit_id or debit_id:
-                _logger.info("Skip creation of journal %s because we didn't found debit/credit account")
+        for name, code, type, tax, partner, account, tag in journals_data:
+            if not account:
+                _logger.info("Skip creation of journal %s because we didn't found default account")
                 continue
             # journal_data.append({
             self.env['account.journal'].create({
@@ -99,9 +92,8 @@ class AccountChartTemplate(models.Model):
                 'tax_settlement': type,
                 'settlement_tax': tax,
                 # 'settlement_financial_report_id': report and report.id,
-                'settlement_partner_id': partner and partner.id,
-                'default_credit_account_id': credit_id,
-                'default_debit_account_id': debit_id,
+                'settlement_partner_id': partner and partner.id or False,
+                'default_account_id': account.id,
                 'company_id': company.id,
                 # al final hicimos otro dashboard
                 'show_on_dashboard': False,
