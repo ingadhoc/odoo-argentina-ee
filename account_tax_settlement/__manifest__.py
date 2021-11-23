e4b5542e9637ecc23015aad1240e42754b78a46e
raguero </SUlem@2017/>
2021-11-22 22:36:51 -0300

    [MIG] account_tax_settlement: Migration to 15.0

--- account_tax_settlement/__manifest__.py
+++ account_tax_settlement/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Tax Settlement',
-    'version': '13.0.1.2.0',
+    'version': "15.0.1.0.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
@@ -30,7 +30,7 @@
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'application': False,
 }
