7176bdca36e2eebf38fc205d99fa646850a044b1
raguero </SUlem@2017/>
2021-11-22 22:30:44 -0300

    [MIG] l10n_ar_currency_update/: Migration to 15.0

--- l10n_ar_currency_update/__manifest__.py
+++ l10n_ar_currency_update/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Argentinian Currency Rate Update',
-    'version': '13.0.1.1.0',
+    'version': "15.0.1.0.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA,Odoo Community Association (OCA)',
@@ -35,7 +35,7 @@
         'data/res_company_data.xml',
         'wizards/res_config_settings_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'application': False,
 }
