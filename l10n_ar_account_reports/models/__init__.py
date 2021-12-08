5acf0757a1f676d0f3ef68aa67ef6d784e208aa8
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-12-08 14:51:23 -0300

    [MIG] l10n_ar_account_reports: Migration to 15.0

--- l10n_ar_account_reports/models/__init__.py
+++ l10n_ar_account_reports/models/__init__.py
@@ -2,6 +2,4 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import account_report
 from . import account_journal
-from . import account_generic_tax_report
