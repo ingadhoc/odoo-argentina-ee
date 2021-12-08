08e1a5e2b54240e1eee2bcbe89d071537f69cd2c
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-12-08 16:32:55 -0300

    [MIG] account_accountant_ux: Migration to 15.0

--- account_accountant_ux/wizards/account_change_lock_date.py
+++ account_accountant_ux/wizards/account_change_lock_date.py
@@ -2,7 +2,8 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class AccountChangeLockDate(models.TransientModel):
@@ -22,9 +23,12 @@ class AccountChangeLockDate(models.TransientModel):
         self.tax_lock_date = self.company_id.tax_lock_date
 
     def change_lock_date(self):
-        self.company_id.sudo().write({
-            'period_lock_date': self.period_lock_date,
-            'fiscalyear_lock_date': self.fiscalyear_lock_date,
-            'tax_lock_date': self.tax_lock_date,
-        })
+        if self.user_has_groups('account.group_account_manager'):
+            self.company_id.sudo().write({
+                'period_lock_date': self.period_lock_date,
+                'fiscalyear_lock_date': self.fiscalyear_lock_date,
+                'tax_lock_date': self.tax_lock_date,
+            })
+        else:
+            raise UserError(_('Only Billing Administrators are allowed to change lock dates!'))
         return {'type': 'ir.actions.act_window_close'}
