64199647b3419f55ea5bbd6fa01f3d54103ebfd5
Pablo Montenegro <pam@adhoc.com.ar>
2021-12-28 18:29:26 +0000

    [IMP] l10n_ar_account_reports: show partner vat
    Show partner vat next to the partner name
    
    X-original-commit: b172a55f228fe80d33f01f78b956eae341d05a1b

--- l10n_ar_account_reports/models/__init__.py
+++ l10n_ar_account_reports/models/__init__.py
@@ -3,3 +3,8 @@
 # directory
 ##############################################################################
 from . import account_journal
+<<<<<<< HEAD
+=======
+from . import account_generic_tax_report
+from . import account_partner_ledger
+>>>>>>> 354bc41... temp
