e620d93987d08bb43546bf55adaa029be431919a
Juan José Scarafía <jjs@adhoc.com.ar>
2022-01-20 15:42:38 +0000

    Update __init__.py
    Part-of: ingadhoc/odoo-argentina-ee#86

--- l10n_ar_account_reports/models/__init__.py
+++ l10n_ar_account_reports/models/__init__.py
@@ -3,8 +3,4 @@
 # directory
 ##############################################################################
 from . import account_journal
-<<<<<<< HEAD
-=======
-from . import account_generic_tax_report
 from . import account_partner_ledger
->>>>>>> 354bc41... temp
