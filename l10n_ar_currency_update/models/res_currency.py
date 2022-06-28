dee323fe78af5d736208ebeb1e4f01d59b8f2b72
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-06-28 21:50:14 +0000

    [FIX] currency_update: tests
    closes ingadhoc/odoo-argentina-ee#125
    
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- l10n_ar_currency_update/models/res_currency.py
+++ l10n_ar_currency_update/models/res_currency.py
@@ -4,6 +4,6 @@ from odoo import _, api, models
 class ResCurrency(models.Model):
     _inherit = 'res.currency'
 
-    @api.onchange('name','l10n_ar_afip_code','symbol')
+    @api.onchange('name', 'l10n_ar_afip_code', 'symbol')
     def onchange_currency_name(self):
         return {'warning': {'title': _('Warning'), 'message': _('Cambiar el nombre de la moneda puede repercutir tanto en el servicio de la sincronización automática de tasa de cambio como en el servicio de la facturación electrónica.')}}
