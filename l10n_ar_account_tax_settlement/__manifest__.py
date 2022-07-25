204aad60b9ccf43d700733b4b06fbe64a985e529
Pablo Montenegro <pam@adhoc.com.ar>
2022-07-25 19:53:30 +0000

    [IMP] l10n_ar_account_tax_settlement: update inflation indexes
    closes ingadhoc/odoo-argentina-ee#132
    
    X-original-commit: f0d1d085426c3a89dab46e6b543eb2d37d43fa43
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>

--- l10n_ar_account_tax_settlement/__manifest__.py
+++ l10n_ar_account_tax_settlement/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Tax Settlements For Argentina',
-    'version': "15.0.1.1.0",
+    'version': "15.0.1.2.0",
     'category': 'Accounting',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
