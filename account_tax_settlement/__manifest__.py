576be90f874a0060259a5cd889bc20e3e2bbaa03
Pablo Montenegro <pam@adhoc.com.ar>
2022-11-03 11:26:34 -0300

    [MIG] account_tax_settlement: Migration to 16.0

--- account_tax_settlement/__manifest__.py
+++ account_tax_settlement/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Tax Settlement',
-    'version': '15.0.1.3.0',
+    'version': "16.0.1.0.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
@@ -31,7 +31,7 @@
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'application': False,
 }
