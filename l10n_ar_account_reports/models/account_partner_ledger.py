14d7af83f53c526d2ff38f8516cd5996b41b5ee0
Maximiliano Mezzavilla <mem@adhoc.com>
2022-11-10 17:16:59 +0000

    [MIG] l10n_ar_account_reports: Migration to 16.0
    closes ingadhoc/odoo-argentina-ee#154
    
    Signed-off-by: Nacho Cainelli <ica@adhoc.com.ar>

--- l10n_ar_account_reports/models/account_partner_ledger.py
+++ l10n_ar_account_reports/models/account_partner_ledger.py
@@ -1,20 +1,20 @@
 from odoo import models, api
 
 
-class ReportPartnerLedger(models.AbstractModel):
-    _inherit = 'account.partner.ledger'
+class PartnerLedgerCustomHandler(models.AbstractModel):
+    _inherit = 'account.partner.ledger.report.handler'
 
 
     @api.model
-    def _get_report_line_partner(self, options, partner, initial_balance, debit, credit, balance):
+    def _get_report_line_partners(self, options, partner, partner_values):
         """ We extend this method in order to show more information aside the partner's name: we add the
         identification type name and the related document number:
 
         * before the partner was shown as "Partner Name"
         * with this method now will be shown as "Partner Name (CUIT: 1234234235)"
         """
-        res = super()._get_report_line_partner(
-            options=options, partner=partner, initial_balance=initial_balance, debit=debit, credit=credit, balance=balance)
+        res = super()._get_report_line_partners(
+            options=options, partner=partner,partner_values=partner_values)
         partner_name = res.get("name")
         if partner and partner.l10n_latam_identification_type_id.name and partner.vat:
             doc_info = partner.l10n_latam_identification_type_id.name + ": " + partner.vat
