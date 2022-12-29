cbfd740ac686e8df0a5da78cdc1d428c4275e49c
Pablo Montenegro <pam@adhoc.com.ar>
2022-12-29 12:27:37 +0000

    [FIX]account_tax_settlement: optimizar el dominio de account.payment.group para tenes en cuenta las líneas a pagar
    closes ingadhoc/odoo-argentina-ee#168
    
    Ticket: 57510
    X-original-commit: fc75b5601cd56ac5a29b3bad910c63f3fad13d7f
    Related: ingadhoc/odoo-public-administration#467
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- account_tax_settlement/models/__init__.py
+++ account_tax_settlement/models/__init__.py
@@ -4,3 +4,4 @@ from . import account_move
 from . import account_journal_dashboard
 from . import account_financial_html_report_line
 from . import account_report
+from . import account_payment_group
