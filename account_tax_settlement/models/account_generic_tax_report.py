37b835cc58c7fc5b172b02f577757cc35394a82f
Maximiliano Mezzavilla <mem@adhoc.com>
2023-04-14 16:37:02 +0000

    [IMP] account_tax_settlement: Se borra boton tax closing en tax report
    closes ingadhoc/odoo-argentina-ee#197
    
    Task: 29975
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>

---
+++ account_tax_settlement/models/account_generic_tax_report.py
@@ -0,0 +1,12 @@
+from odoo import models
+
+
+class GenericTaxReportCustomHandler(models.AbstractModel):
+
+    _inherit = 'account.generic.tax.report.handler'
+
+    def _custom_options_initializer(self, report, options, previous_options=None):
+        super()._custom_options_initializer(report, options, previous_options=previous_options)
+        for x in (options['buttons']):
+            if x.get('action') == 'action_periodic_vat_entries':
+                options['buttons'].remove(x)
