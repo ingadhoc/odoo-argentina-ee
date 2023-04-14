657e08c2632cb6a0ea44a634d5f791a3cd9c58c6
Pablo Montenegro <pam@adhoc.com.ar>
2023-04-14 13:26:55 -0300

    [FIX] l10n_ar_account_tax_settlement: fix payment_date field does not exists on account.payment model Ticket: 60800

--- l10n_ar_account_tax_settlement/models/account_journal.py
+++ l10n_ar_account_tax_settlement/models/account_journal.py
@@ -1142,7 +1142,7 @@ class AccountJournal(models.Model):
                 content += '%016d' % int(re.sub('[^0-9]', '', move.l10n_latam_document_number))
                 #Importe del comprobante
                 codop = '1'
-                issue_date = payment.payment_date
+                issue_date = payment.date
                 amount_tot = abs(payment.payment_group_id.payments_amount)
                 # withholdable_base_amount es para ret de gcias, withholding_base_amount es para ret de iva
                 base_amount = payment.withholdable_base_amount or payment.withholding_base_amount
@@ -1252,7 +1252,7 @@ class AccountJournal(models.Model):
         content = ''
         for line in move_lines.sorted(key=lambda r: (r.date, r.id)):
             if line.payment_id:
-                date = line.payment_id.payment_date
+                date = line.payment_id.date
                 content += line.partner_id.ensure_vat()
                 content += line.partner_id.name.ljust(80)[:80]
                 content += '%010d' % int(line.name)
@@ -1276,7 +1276,7 @@ class AccountJournal(models.Model):
             payment = line.payment_id
             if payment:
                 # Fecha
-                content += fields.Date.from_string(payment.payment_date).strftime('%d-%m-%Y') + ','
+                content += fields.Date.from_string(payment.date).strftime('%d-%m-%Y') + ','
 
                 # Constancia
                 content += payment.withholding_number[-8:] + ','
