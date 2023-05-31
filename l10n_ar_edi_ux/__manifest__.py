1b06fabcdd11181032c94d666d95861ca07b2280
Martin Quinteros <maq@adhoc.com.ar>
2023-05-31 19:35:45 +0000

    [IMP] l10n_ar_edi_ux: Change AFIP WS Padron
    from ws_sr_padron_a5 to ws_sr_constancia_inscripcion
    
    Manual backport
    
    X-original-commit: 419569ca34d25f0552ea1445ea2e0731a4933395

--- l10n_ar_edi_ux/__manifest__.py
+++ l10n_ar_edi_ux/__manifest__.py
@@ -1,6 +1,12 @@
 {
     'name': 'Argentinian Electronic Invoicing UX',
+<<<<<<< HEAD
     'version': "16.0.1.1.0",
+||||||| parent of 79dcf4c (temp)
+    'version': "15.0.1.2.0",
+=======
+    'version': "15.0.1.3.0",
+>>>>>>> 79dcf4c (temp)
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
