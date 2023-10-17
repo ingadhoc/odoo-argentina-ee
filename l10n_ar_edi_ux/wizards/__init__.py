29847287b00965f077edf72daedd62476f7b38e5
Maximiliano Mezzavilla <mem@adhoc.com.ar>
2023-10-17 16:43:03 +0000

    [FIX] l10n_ar_edi_ux: The buttotns are being deleted due to disuse.
    closes ingadhoc/odoo-argentina-ee#312
    
    Task: 28319
    X-original-commit: eb2e8b3e6cd6e22b7ccd07bd62d97a854385b677
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>

--- l10n_ar_edi_ux/wizards/__init__.py
+++ l10n_ar_edi_ux/wizards/__init__.py
@@ -3,5 +3,4 @@
 # directory
 ##############################################################################
 from . import res_partner_update_from_padron_wizard
-from . import res_config_settings
 from . import account_check_action_wizard
--- l10n_ar_edi_ux/wizards/res_config_settings.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError
-import requests
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    arba_cit = fields.Char(related='company_id.arba_cit', readonly=False)
