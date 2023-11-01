373a639f97d721673b6d1d0cf5051af4d19bfa0c
Martin Quinteros <maq@adhoc.com.ar>
2023-11-01 20:14:47 +0000

    [IMP] Add amount residual currency in _search_filter_amount
    closes ingadhoc/odoo-argentina-ee#281
    
    Signed-off-by: Ignacio Cainelli <ica@adhoc.com.ar>

--- account_accountant_ux/models/account_move_line.py
+++ account_accountant_ux/models/account_move_line.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, osv
 
 
 class AccountMovetLine(models.Model):
@@ -6,20 +6,35 @@ class AccountMovetLine(models.Model):
     _inherit = 'account.move.line'
 
     filter_amount = fields.Float(compute="compute_filter_amout", search='_search_filter_amount')
-    
+
     def compute_filter_amout(self):
         self.filter_amount = False
 
     def _search_filter_amount(self, operator, value):
         res = []
         if self.env.context.get('default_st_line_id'):
-            amount = self.env['account.bank.statement.line'].browse(self.env.context.get('default_st_line_id'))['amount'] 
+            statement_line = self.env['account.bank.statement.line'].browse(self.env.context.get('default_st_line_id'))
+
+            if statement_line.company_id.currency_id == statement_line.journal_id.currency_id:
+                amount = statement_line['amount']
+                amount_currency = statement_line['amount_currency']
+            else:
+                amount = statement_line['amount_currency']
+                amount_currency = statement_line['amount']
+
             if value != 0 and operator == '=':
                 base_amount = ((100 - value)/100) * amount
                 top_amount = ((100 + value)/100) * amount
-                res.append(('amount_residual', '>',  base_amount))
-                res.append(('amount_residual', '<',  top_amount))
+                base_amount_currency = ((100 - value)/100) * amount_currency
+                top_amount_currency = ((100 + value)/100) * amount_currency
+                res += osv.expression.OR([
+                    [('amount_residual', '>',  base_amount),('amount_residual', '<',  top_amount)],
+                    [('amount_residual_currency', '>',  base_amount_currency),('amount_residual_currency', '<',  top_amount_currency)]
+                ])
             else:
                 amount = ((100 - value)/100) * amount
+                amount_currency = ((100 - value)/100) * amount_currency
+                res.append('|')
                 res.append(('amount_residual', operator,  amount))
+                res.append(('amount_residual_currency', operator,  amount_currency))
         return res
