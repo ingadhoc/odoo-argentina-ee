b437bba4c53961fbb67138f42b92892e5ca4ad54
Pablo Montenegro <pam@adhoc.com.ar>
2023-11-24 13:16:49 +0000

    [MIG] account_tax_settlement: Migration to 17.0
    Part-of: ingadhoc/odoo-argentina-ee#287

--- account_tax_settlement/__manifest__.py
+++ account_tax_settlement/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Tax Settlement',
-    'version': "16.0.1.2.0",
+    'version': "17.0.1.0.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
@@ -31,7 +31,7 @@
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'application': False,
 }
