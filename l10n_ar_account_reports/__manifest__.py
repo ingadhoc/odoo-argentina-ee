f95c85ce2318720c514c2997f0164cb2452049c8
Pablo Montenegro <pam@adhoc.com.ar>
2024-02-01 18:55:41 -0300

    [FIX] l10n_ar_account_reports: bug with traceback cheques a fecha Al momento de abrir el informe de cheques a fecha se obtiene un bug con traceback debido a que en este pr https://github.com/ingadhoc/odoo-argentina-ee/pull/198 no hubo cambio en la versión del manifest. Ticket: 70622

--- l10n_ar_account_reports/__manifest__.py
+++ l10n_ar_account_reports/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Accounting Reports Customized for Argentina',
-    'version': "16.0.1.1.0",
+    'version': "16.0.1.2.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
