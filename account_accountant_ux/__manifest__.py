e02ead30b71644d409deb9d5237e425660159801
Camila Vives <cav@adhoc.com.ar>
2024-03-04 16:41:53 -0300

    [FIX]account_accountant_ux: replace 'active_id' for 'id'

--- account_accountant_ux/__manifest__.py
+++ account_accountant_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Accounting Accountant UX',
-    'version': "16.0.2.2.0",
+    'version': "16.0.2.3.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
