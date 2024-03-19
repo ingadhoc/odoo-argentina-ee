04ca551dabe20779ec6c6bdcd6da67e8620a6467
Juan José Scarafia <jjs@adhoc.com.ar>
2024-03-19 20:13:46 +0000

    [FIX] account_report inheritance
    closes ingadhoc/odoo-argentina-ee#351
    
    X-original-commit: b5be40e681b3d2994e3d3e0d1e7438b53098c03a
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- account_tax_settlement/models/account_report.py
+++ account_tax_settlement/models/account_report.py
@@ -4,7 +4,7 @@ from odoo.exceptions import ValidationError
 from odoo.osv import expression
 
 
-class AccountReport(models.AbstractModel):
+class AccountReport(models.Model):
     _inherit = 'account.report'
 
     allow_settlement = fields.Boolean(
