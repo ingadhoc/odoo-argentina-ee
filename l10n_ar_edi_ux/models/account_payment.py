ae3de570117c38442496fa6fd21502973da7c6e7
Camila Vives <cav@adhoc.com.ar>
2024-04-17 12:40:56 -0300

    [REF] l10n_ar_edi_ux: remove check_debit_journal_id

--- l10n_ar_edi_ux/models/account_payment.py
+++ l10n_ar_edi_ux/models/account_payment.py
@@ -4,6 +4,4 @@ from odoo import fields, models
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
-    # Diario de tipo varios que está vinculado al diario del pago del cheque (ver campo Check Debit Journal en el diario) para que nos permita hacer el débito del cheque.
-    # Lo utilizamos como un diario "puente". Si no está establecido entonces usamos este campo para hacer invisible el botón para debitar cheques en la vista form del payment (es decir, del cheque).
-    check_debit_journal_id = fields.Many2one(related='journal_id.check_debit_journal_id', readonly=True)
+    check_add_debit_button = fields.Boolean(related='journal_id.check_add_debit_button', readonly=True)
