7195662b6fa944fc67fd27770d609c86e0b85b54
Maximiliano Mezzavilla <mem@adhoc.com.ar>
2024-04-22 13:29:37 -0300

    [FIX] l10n_ar_edi_ux: We clean the dictionaries with None values
    Task: 29136
    X-original-commit: 69ab8e17eb200551cf0050fd707aa7ef0e20093c

--- l10n_ar_edi_ux/models/res_partner.py
+++ l10n_ar_edi_ux/models/res_partner.py
@@ -25,6 +25,77 @@ class ResPartner(models.Model):
         self.ensure_one()
         return True
 
+    def _clean_response_obj(self, xml_tag, default_replace={}):
+        """
+        Este método procesa el response que nos devuelve afip para reemplazar los valores `None` por un valor predeterminado
+        basado en el tipo de dato especificado. Si el response en sí es `None` o está vacío,
+        lo reemplaza completamente con el valor indicado en el parámetro `type_replace`.
+
+        El mapeo de tipos de datos y sus valores predeterminados se define en la variable interna `replace_values`.
+
+        :param xml_tag: El respopnse o valor que se debe procesar.
+                        Si está vacío o es `None`, se reemplaza completamente por `type_replace`.
+        :param type_replace: Valor que se usará como reemplazo global si el diccionario está vacío o es `None`.
+        :return: Diccionario modificado con los valores `None` reemplazados según el tipo especificado.
+        """
+        res = {}
+        replace_values = {
+            'datosGenerales': {},
+            'caracterizacion': [],
+            'domicilioFiscal': {},
+            'codPostal': '',
+            'descripcionProvincia': '',
+            'direccion': '',
+            'idProvincia': '',
+            'localidad': '',
+            'tipoDomicilio': '',
+            'datoAdicional': '',
+            'tipoDatoAdicional': '',
+            'esSucesion': '',
+            'estadoClave': '',
+            'apellido': '',
+            'dependencia': '',
+            'nombre': '',
+            'razonSocial': '',
+            'tipoClave': '',
+            'tipoPersona': '',
+            'datosMonotributo': {},
+            'datosRegimenGeneral': {},
+            'actividad': [],
+            'impuesto': [],
+            'regimen': [],
+            'errorConstancia': '',
+            'errorMonotributo': '',
+            'errorRegimenGeneral': '',
+            'descripcionActividad': '',
+            'descripcionImpuesto': '',
+            'descripcionRegimen': '',
+            'tipoRegimen': '',
+            'metadata': {},
+            'servidor': '',
+        }
+
+        # Si el diccionario es None o vacío, lo reemplazamos por el valor dado en type_replace
+        if not xml_tag:
+            return default_replace
+
+        # Si el diccionario tiene valores, recorremos y reemplazamos los valores None
+        # Si el valor en si es un diccionario lo limpiamops tambien
+        if isinstance(xml_tag, dict):  # Procesar si es un diccionario
+            cleaned = {}
+            for key, value in xml_tag.items():
+                type_replace = replace_values.get(key, default_replace)
+                if isinstance(value, dict):
+                    cleaned[key] = self._clean_response_obj(value, default_replace=type_replace)
+                elif isinstance(value, list):
+                    cleaned[key] = [
+                        self._clean_response_obj(item, default_replace=type_replace) if isinstance(item, (dict, list)) else (type_replace if item is None else item)
+                        for item in value
+                    ]
+                else:
+                    cleaned[key] = type_replace if value is None else value
+            return cleaned
+
     def get_data_from_padron_afip(self):
         self.ensure_one()
         vat = self.ensure_vat()
@@ -57,8 +128,13 @@ class ResPartner(models.Model):
 
         if errors:
             raise UserError(error_msg % (self.name, vat, errors))
+        
+        # Serializamos una sola vez
+        res = serialize_object(res, dict)
+        res = self._clean_response_obj(res)
 
-        data = serialize_object(res.datosGenerales, dict)
+        data = res.get('datosGenerales')
+
         if not data:
             raise UserError(error_msg % (self.name, vat, res))
 
@@ -66,9 +142,10 @@ class ResPartner(models.Model):
         if not denominacion or denominacion == ', ':
             raise UserError(error_msg % (self.name, vat, 'La afip no devolvió nombre'))
 
-        domicilio = data.get("domicilioFiscal", {})
-        data_mt = serialize_object(res.datosMonotributo, dict) or {}
-        data_rg = serialize_object(res.datosRegimenGeneral, dict) or {}
+        domicilio = data.get("domicilioFiscal")
+        data_mt = res.get('datosMonotributo')
+        data_rg = res.get('datosRegimenGeneral')
+
         impuestos = [imp["idImpuesto"]
                      for imp in data_mt.get("impuesto", []) + data_rg.get("impuesto", [])
                      if data.get('estadoClave') == 'ACTIVO']
@@ -83,7 +160,7 @@ class ResPartner(models.Model):
         def check_activity(data_rg, data_mt):
             res = []
             new_activity = {}
-            afip_activities = data_rg.get("actividad", []) + ([data_mt.get("actividadMonotributista")] if data_mt else [])
+            afip_activities = data_rg.get("actividad", []) + ([data_mt.get("actividadMonotributista", [])] if data_mt else [])
             actividades = self.env['afip.activity'].sudo()
             activity_codes = actividades.search([]).mapped('code')
             for act in afip_activities:
