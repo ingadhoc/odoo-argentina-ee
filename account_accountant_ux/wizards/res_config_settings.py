27694b7812f3869ef3d48d0f9c525487e30bde11
Camila Vives <cav@adhoc.com.ar>
2024-04-29 11:56:00 -0300

    [IMP] account_accountant_ux: add amount filter config param

--- account_accountant_ux/wizards/res_config_settings.py
+++ account_accountant_ux/wizards/res_config_settings.py
@@ -8,3 +8,8 @@ class ResConfigSettings(models.TransientModel):
         related='company_id.use_company_currency_on_followup',
         readonly=False,
     )
+
+    use_search_filter_amount = fields.Boolean(
+        config_parameter='account_accountant_ux.use_search_filter_amount',
+        readonly=False,
+        )
