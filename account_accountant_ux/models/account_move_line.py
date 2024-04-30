7c6857515c7067f1ea680e94d719eba7fb03cd6d
Camila Vives <cav@adhoc.com.ar>
2024-04-30 18:23:53 +0000

    [FIX] account_accountant_ux: fix filter by same amount
    X-original-commit: f6495b522c48a9bd4396d1c5f8091c1b6eb51890

--- account_accountant_ux/models/account_move_line.py
+++ account_accountant_ux/models/account_move_line.py
@@ -13,7 +13,16 @@ class AccountMovetLine(models.Model):
     def _search_filter_amount(self, operator, value):
         res = []
         if self.env.context.get('default_st_line_id'):
-            amount = self.env['account.bank.statement.line'].browse(self.env.context.get('default_st_line_id'))['amount'] 
+            statement_line = self.env['account.bank.statement.line'].browse(self.env.context.get('default_st_line_id'))
+
+            primary_currency = statement_line.company_id.currency_id == statement_line.currency_id
+            if primary_currency:
+                amount = statement_line['amount']
+                amount_currency = statement_line['amount_currency']
+            else:
+                amount = statement_line['amount_currency']
+                amount_currency = statement_line['amount']
+
             if value != 0 and operator == '=':
                 base_amount = ((100 - value)/100) * amount
                 top_amount = ((100 + value)/100) * amount
@@ -21,5 +30,10 @@ class AccountMovetLine(models.Model):
                 res.append(('amount_residual', '<',  top_amount))
             else:
                 amount = ((100 - value)/100) * amount
-                res.append(('amount_residual', operator,  amount))
+                amount_currency = ((100 - value)/100) * amount_currency
+                
+                if primary_currency:
+                    res.append(('amount_residual', operator,  amount))
+                else:
+                    res.append(('amount_residual', operator,  amount_currency))
         return res
