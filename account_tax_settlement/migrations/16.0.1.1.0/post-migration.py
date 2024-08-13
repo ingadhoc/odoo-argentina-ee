a6fdf6bf348f6d465b2667bbaef4a4f41c950e32
Nicolas Mac Rouillon <nicolasmacr@gmail.com>
2024-08-13 18:34:30 +0000

    [FIX] account_tax_settlement: changes on migracion script load_data
    closes ingadhoc/odoo-argentina-ee#434
    
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- account_tax_settlement/migrations/16.0.1.1.0/post-migration.py
+++ account_tax_settlement/migrations/16.0.1.1.0/post-migration.py
@@ -4,4 +4,4 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(
-        env.cr, 'account_tax_settlement', 'data/account_report_data.xml')
+        env, 'account_tax_settlement', 'data/account_report_data.xml')
