4538b58eba7011bf2c582810c644d58de3007ee4
Katherine Zaoral <kz@adhoc.com.ar>
2024-09-24 13:43:20 -0300

    [ADD] l10n_ar_currency_update: Unit test currency rate
    We modify and activate the test so can be run and check that
    we are properly processing the rates form AFIP

--- l10n_ar_currency_update/tests/test_l10n_ar_currency_update.py
+++ l10n_ar_currency_update/tests/test_l10n_ar_currency_update.py
@@ -2,39 +2,45 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo.tests import common, tagged
+import datetime
+from unittest import mock
+from unittest.mock import patch
+
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 
-@tagged('-at_install', '-standard', 'currency_update', 'post_install')
-class TestL10nArCurrencyUpdate(common.TransactionCase):
+class TestL10nArCurrencyUpdate(AccountTestInvoicingCommon):
 
-    # TODO when runing this test please update this values to the day rate
+    @classmethod
+    def setUpClass(cls, chart_template_ref="ar_ri"):
+        super().setUpClass(chart_template_ref=chart_template_ref)
+        cls.ARS = cls.env.ref('base.ARS')
+        cls.USD = cls.env.ref('base.USD')
+        cls.EUR = cls.env.ref('base.EUR')
 
-    def setUp(self):
-
-        self.ARS_USD = 1 / 128.178
-        self.ARS_EUR = 1 / 60.178
-        # sin uso por ahora
-        # self.USD_EUR = self.ARS_USD / self.ARS_EUR
-
-        super().setUp()
-
-        self.ARS = self.env.ref('base.ARS')
-        self.USD = self.env.ref('base.USD')
-        self.EUR = self.env.ref('base.EUR')
-
-        # activamos monedas por las dudas
-        self.USD.active = True
-        self.EUR.active = True
+        # Activamos monedas por las dudas
+        cls.USD.active = True
+        cls.EUR.active = True
+        cls.utils_path = "odoo.addons.l10n_ar_currency_update.models.res_company.ResCompany"
 
     def test_ARS(self):
         """ When the base currency is ARS """
-        company = self.env.ref('l10n_ar.company_ri')
-        company.currency_id = self.ARS
-        self.env.company = company
+        msg_error = "Should not be any rate for this currency and company to continue with the test"
+        self.assertEqual(self.env.company.currency_id, self.ARS)
+        self.assertEqual(self.ARS.rate, 1.0, msg_error)
+        self.assertEqual(self.USD.rate, 1.0, msg_error)
+        self.assertEqual(self.EUR.rate, 1.0, msg_error)
 
-        company.update_currency_rates()
+        test_date = datetime.date(2024, 9, 24)
+        mocked_res = {
+            'ARS': (1.0, test_date),
+            'EUR': (0.0009435361546070796, test_date),
+            'USD': (0.0010481301358376655, test_date),
+        }
+
+        with patch(f"{self.utils_path}._parse_afip_data", return_value=mocked_res):
+            self.env.company.update_currency_rates()
 
         self.assertEqual(self.ARS.rate, 1.0)
-        self.assertAlmostEqual(self.USD.rate, self.ARS_USD, places=3)
-        self.assertAlmostEqual(self.EUR.rate, self.ARS_EUR, places=3)
+        self.assertNotEqual(self.USD.rate, 954.08)
+        self.assertNotEqual(self.EUR.rate, 1059.8428)
