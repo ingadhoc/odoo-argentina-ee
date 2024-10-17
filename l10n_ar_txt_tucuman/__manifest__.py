bfbd95eb2a81498d87acade55723dd2913781387
Pablo Montenegro <pam@adhoc.com.ar>
2024-10-17 15:19:24 -0300

    [IMP] l10n_ar_txt_tucuman: new module Tarea: 38200

---
+++ l10n_ar_txt_tucuman/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Txt Tucuman',
+    'version': "16.0.1.0.0",
+    'category': 'Accounting',
+    'website': 'www.adhoc.com.ar',
+    'license': 'LGPL-3',
+    'depends': [
+        'l10n_ar_account_tax_settlement',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
