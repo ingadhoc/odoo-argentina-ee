4cc32f69ea67283b26bd180acf7462750558be68
docker-odoo <docker-odoo@example.com>
2024-12-17 12:32:39 +0000

    [MIG] l10n_ar_account_tax_settlement: Migration to 18.0

--- l10n_ar_account_tax_settlement/__manifest__.py
+++ l10n_ar_account_tax_settlement/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Tax Settlements For Argentina',
-    'version': "17.0.1.4.0",
+    'version': "18.0.1.0.0",
     'category': 'Accounting',
     'website': 'www.adhoc.com.ar',
     'license': 'LGPL-3',
@@ -43,7 +43,7 @@
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'application': False,
 }
