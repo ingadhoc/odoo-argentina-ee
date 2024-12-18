534ec5715699c0be55e277971bb5a0be0cdf304f
Camila Vives <cav@adhoc.com.ar>
2024-12-18 19:58:12 +0000

    [FIX] account_batch_payment_ux: change sequence in payment view
    X-original-commit: 83fba2db7933283a530fb7b0011f70577ec16027

--- account_batch_payment_ux/__manifest__.py
+++ account_batch_payment_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Account Batch Payment UX',
-    'version': "17.0.0.1.0",
+    'version': "17.0.0.2.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
@@ -33,6 +33,7 @@
     ],
     'data': [
         'views/account_batch_payment.xml',
+        'views/account_payment_views.xml'
     ],
     'demo': [
     ],
