f684e847077f6d67647520922cfaf58db92996a7
Katherine Zaoral <kz@adhoc.com.ar>
2025-01-10 19:08:16 +0000

    [FIX] l10n_ar_edi_ux: avoid access error
    l10n_ar_afip_ws_crt field can only be access by administrators.
    We take this into account, but we miss to also made the exception
    on the search() method.
    
    Ticket 86102
    
    closes ingadhoc/odoo-argentina-ee#528
    
    X-original-commit: 364c68efb78358b18d25abcffd68b362ffc711ff
    Signed-off-by: Julia Elizondo <jue@adhoc.com.ar>
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>

--- l10n_ar_edi_ux/models/res_partner.py
+++ l10n_ar_edi_ux/models/res_partner.py
@@ -101,9 +101,10 @@ class ResPartner(models.Model):
         vat = self.ensure_vat()
 
         # if there is certificate for current company use that one, if not use the company with first certificate found
-        valid_certificate = self.env['certificate.certificate'].search(
+        today = fields.Date.context_today(self.with_context(tz='America/Argentina/Buenos_Aires'))
+        valid_certificate = self.env['certificate.certificate'].sudo().search(
             [('active', '=', True), ('date_end', '>=', today), ("country_code", "=", "AR")])
-        if self.env.company.l10n_ar_afip_ws_crt_id in valid_certificate:
+        if self.env.company.sudo().l10n_ar_afip_ws_crt_id in valid_certificate:
             company = self.env.company
         else:
             company = valid_certificate[:1].company_id if valid_certificate else False
