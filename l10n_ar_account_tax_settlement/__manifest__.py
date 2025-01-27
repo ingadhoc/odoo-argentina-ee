b4d3e1e03b0753d54806af3697304574c482eae4
Julia Elizondo <jue@adhoc.com.ar>
2025-01-27 19:28:35 +0000

    [IMP] l10n_ar_tax_settlement: added author on manifest
    Part-of: ingadhoc/odoo-argentina-ee#540

--- l10n_ar_account_tax_settlement/__manifest__.py
+++ l10n_ar_account_tax_settlement/__manifest__.py
@@ -21,6 +21,7 @@
     'name': 'Tax Settlements For Argentina',
     'version': "18.0.1.0.0",
     'category': 'Accounting',
+    'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'LGPL-3',
     'images': [
