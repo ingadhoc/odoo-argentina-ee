eaa95a4e38e6a693c5c6ffa830a3a38a7fadec98
Julia Elizondo <jue@adhoc.com.ar>
2025-01-27 19:27:03 +0000

    [IMP] l10n_ar_edi_ux: import monkey_patches instead of *
    Part-of: ingadhoc/odoo-argentina-ee#540

--- l10n_ar_edi_ux/__init__.py
+++ l10n_ar_edi_ux/__init__.py
@@ -4,4 +4,4 @@
 ##############################################################################
 from . import models
 from . import wizards
-from .monkey_patches import *
+from .monkey_patches import monkey_patches
