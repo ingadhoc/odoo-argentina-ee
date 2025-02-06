58adee7781a6877964ca2419cd5b3c4e9d85f4f4
Martin Quinteros <maq@adhoc.com.ar>
2025-02-06 09:05:55 -0300

    [ADD] Add new module l10n_ar_edi_payment_pro to re-implement ws logic
      for argentina debit/credit notes generated from payment form
    
    X-original-commit: a58d11fb776e95e72c09a1b5e355ac7badd37a7c

---
+++ l10n_ar_edi_payment_pro/__manifest__.py
@@ -0,0 +1,25 @@
+# © 2025 ADHOC SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Payment pro for l10n_ar_edi",
+    "version": "18.0.1.0.0",
+    "category": "Payment",
+    "website": "www.adhoc.com.ar",
+    "author": "ADHOC SA",
+    "license": "AGPL-3",
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "account_payment_pro",
+        "l10n_ar_edi_ux",
+    ],
+    "data": [
+        "wizards/account_payment_invoice_wizard_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+    "demo": [],
+}
