936631ffa96f4227591ae950daaf54790db141b6
Martin Quinteros <maq@adhoc.com.ar>
2025-03-14 20:13:17 +0000

    [IMP] account_accountant_ux: extend auto reconcile wizard adding all from partner strategy
    Part-of: ingadhoc/odoo-argentina-ee#576

---
+++ account_accountant_ux/wizards/account_auto_reconcile_wizard.py
@@ -0,0 +1,49 @@
+from odoo import models, fields, api, _
+
+
+class AccountAutoReconcileWizard(models.TransientModel):
+    _inherit = 'account.auto.reconcile.wizard'
+
+    search_mode = fields.Selection(
+        selection_add=[('all_from_partner', 'All balances from one partner')],
+        ondelete={'all_from_partner': 'set default'}
+    )
+
+    def _auto_reconcile_all_from_partner(self):
+        """ Auto-reconcile with all-to-partner strategy:
+        We will reconcile all amls together gruped by partner.
+        :return: a recordset of reconciled amls
+        """
+        grouped_amls_data = self.env['account.move.line']._read_group(
+            self._get_amls_domain(),
+            ['account_id', 'partner_id', 'currency_id','company_id'],
+            ['id:recordset'],
+        )
+        all_reconciled_amls = self.env['account.move.line']
+        amls_grouped_by_2 = []  # we need to group amls with right format for _reconcile_plan
+        for *__, grouped_aml_ids in grouped_amls_data:
+            positive_amls = grouped_aml_ids.filtered(lambda aml: aml.amount_residual_currency >= 0).sorted('date')
+            negative_amls = (grouped_aml_ids - positive_amls).sorted('date')
+            if positive_amls and negative_amls :
+                all_reconciled_amls += positive_amls + negative_amls
+                amls_grouped_by_2  += [grouped_aml_ids]
+        self.env['account.move.line']._reconcile_plan(amls_grouped_by_2)
+        return all_reconciled_amls
+
+    def auto_reconcile(self):
+        """ Automatically reconcile amls given wizard's parameters.
+        :return: an action that opens all reconciled items and related amls (exchange diff, etc)
+        """
+        self.ensure_one()
+        if self.search_mode == 'all_from_partner':
+            reconciled_amls = self._auto_reconcile_zero_balance()
+            if reconciled_amls:
+                return {
+                    'name': _("Automatically Reconciled Entries"),
+                    'type': 'ir.actions.act_window',
+                    'res_model': 'account.move.line',
+                    'context': "{'search_default_group_by_matching': True}",
+                    'view_mode': 'list',
+                    'domain': [('id', 'in', reconciled_amls.ids)],
+                }
+        super().auto_reconcile()
