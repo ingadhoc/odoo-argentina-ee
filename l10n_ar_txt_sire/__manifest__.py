e3e6bca3d53f9a997f6045076e5d0680f52c23c9
Rocío Vega <rov@adhoc.com.ar>
2025-06-04 20:26:54 +0000

    [FIX] l10n_ar_txt_sire: Fix arquitecture of the payment view
    X-original-commit: 2aca73be9666656a2bb6dc44d3d97dea1081b331

---
+++ l10n_ar_txt_sire/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Txt SIRE',
+    'version': "16.0.1.1.0",
+    'category': 'Accounting',
+    'website': 'www.adhoc.com.ar',
+    'license': 'LGPL-3',
+    'depends': [
+        'l10n_ar_account_tax_settlement',
+        'l10n_ar_ux',
+    ],
+    'data': [
+        'data/account_account_tag.xml',
+        'views/res_partner_view.xml',
+        'views/account_payment_view.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
