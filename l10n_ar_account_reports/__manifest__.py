b0c67ff5908160b7c8fe0be33af0217abde5aea9
Felipe Garcia Suez <feg@adhoc.com.ar>
2025-08-05 14:35:30 +0000

    [REM] l10n_ar_account_reports: Moves checks to date to l10n_latam_check_ux

--- l10n_ar_account_reports/__manifest__.py
+++ l10n_ar_account_reports/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Accounting Reports Customized for Argentina",
-    "version": "18.0.1.3.0",
+    "version": "18.0.1.4.0",
     "category": "Accounting",
     "sequence": 14,
     "summary": "",
@@ -31,16 +31,12 @@
         "account_reports",
         "l10n_ar",
         "l10n_ar_reports",
-        "l10n_latam_check",
     ],
     "data": [
         "data/tags_data.xml",
         "data/estado_resultados.xml",
         "data/balance_sheet.xml",
         "data/account.account.tag.csv",
-        "wizards/checks_to_date_view.xml",
-        "reports/report_checks_to_date.xml",
-        "security/ir.model.access.csv",
     ],
     "demo": [],
     "test": [],
