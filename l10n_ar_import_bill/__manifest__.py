26a59d9b1f4b6b2593fab7b019c9abb7c1b3c0ec
Felipe Garcia Suez <feg@adhoc.com.ar>
2025-08-12 11:33:09 -0300

    [WIP] l10n_ar_import_bill: Update AFIP to ARCA

--- l10n_ar_import_bill/__manifest__.py
+++ l10n_ar_import_bill/__manifest__.py
@@ -1,6 +1,6 @@
 {
-    "name": "Argentinian Importing Bills from AFIP",
-    "version": "18.0.1.0.0",
+    "name": "Argentinian Importing Bills from ARCA",
+    "version": "18.0.1.1.0",
     "category": "Localization/Argentina",
     "sequence": 8,
     "author": "ADHOC SA",
