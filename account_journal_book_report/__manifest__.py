7dd3094812a27ebfed2816838a273814569da636
Rocío Vega <rov@adhoc.com.ar>
2025-11-13 20:31:16 +0000

    [IMP] account_journal_book_report: Improve file download to be done by background
    closes ingadhoc/odoo-argentina-ee#790
    
    X-original-commit: 4dbf76b46c04ee1cfa3f5f2b39999a6ba0454c97
    Signed-off-by: Filoquin adhoc <maq@adhoc.com.ar>
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- account_journal_book_report/__manifest__.py
+++ account_journal_book_report/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Reporte de Libro Diario Contable",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.1.0",
     "author": "ADHOC SA",
     "website": "www.adhoc.com.ar",
     "category": "Localization/Accounting",
@@ -8,6 +8,7 @@
     "depends": [
         "account_reports",
         "report_aeroo",
+        "base_bg",
     ],
     "data": [
         "wizard/account_journal_book_report_views.xml",
