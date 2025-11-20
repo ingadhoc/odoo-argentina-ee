fb301534648172512624152567137098a727a036
Pablo Montenegro <pam@adhoc.com.ar>
2025-11-20 14:37:13 +0000

    [IMP] l10n_ar_account_tax_settlement: ARBA archivo txt A-122R vigente desde 01/03/2026
    Desarrollo de nuevo archivo txt para la presentación de declaraciones juradas de impuestos provinciales de ARBA.
    'TXT Ret IIBB aplicadas ARBA alta por lote A-122R. Vigente desde 01/03/2026.'.
    La especificación del archivo txt se puede ver en readme o en el docstring del método que lo genera o en l10n_ar_account_tax_settlement/doc/arba/especificacion_arba_lotes_retencion_a122rdesde_01032026.png.
    El usuario no debe realizar ninguna configuración extra, el nuevo archivo txt se va a poder descargar si el usuario selecciona el impuesto de liquidación (settlement_tax) "iibb_aplicado_arba_desde_01032026" o "iibb_aplicado_arba_act_7_desde_01032026" en el diario de liquidación de impuestos.
    
    Tarea: 60450
    X-original-commit: 301ce982f21819a09b11839b08a22fed3685e4a8

--- l10n_ar_account_tax_settlement/__manifest__.py
+++ l10n_ar_account_tax_settlement/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Tax Settlements For Argentina",
-    "version": "18.0.1.11.0",
+    "version": "18.0.1.12.0",
     "category": "Accounting",
     "author": "ADHOC SA",
     "website": "www.adhoc.com.ar",
