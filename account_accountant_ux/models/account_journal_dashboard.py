780683418d8d3618cd3535b5ce15baf0fba4a71a
Martin Quinteros <maq@adhoc.com.ar>
2025-11-26 10:18:18 -0300

    [FIX] PERF en account dashboard account_balance_general
    Part-of: ingadhoc/odoo-argentina-ee#807
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- account_accountant_ux/models/account_journal_dashboard.py
+++ account_accountant_ux/models/account_journal_dashboard.py
@@ -11,33 +11,35 @@ class AccountJournal(models.Model):
         return res
 
     def _fill_journal_dashboard_general_balance(self, dashboard_data):
-        journals = self.filtered(lambda journal: journal.type in ["bank", "cash"])
+        journals = self.filtered(lambda journal: journal.type in ["bank", "cash"] and journal.default_account_id)
+        account_ids = journals.mapped("default_account_id").ids
+
+        query = """SELECT aml.account_id, sum(aml.balance) as balance, sum(aml.amount_currency) as amount_currency
+                        FROM account_move_line aml
+                        LEFT JOIN account_move move ON aml.move_id = move.id
+                        WHERE aml.account_id in %(ids)s
+                        AND move.date <= %(date)s AND move.state = 'posted'
+                        GROUP BY aml.account_id"""
+        self.env.cr.execute(
+            query,
+            {
+                "ids": tuple(account_ids),
+                "date": fields.Date.context_today(self),
+            },
+        )
+        query_results = {x["account_id"]: (x["balance"], x["amount_currency"]) for x in self.env.cr.dictfetchall()}
+
         for journal in journals:
-            if journal.default_account_id:
-                amount_field = (
-                    "aml.balance"
-                    if (not journal.currency_id or journal.currency_id == journal.company_id.currency_id)
-                    else "aml.amount_currency"
+            if query_results and journal.default_account_id.id in query_results:
+                if not journal.currency_id or journal.currency_id == journal.company_id.currency_id:
+                    account_sum = query_results[journal.default_account_id.id][0]
+                else:
+                    account_sum = query_results[journal.default_account_id.id][1]
+                currency = journal.currency_id or journal.company_id.currency_id
+                dashboard_data[journal.id].update(
+                    {
+                        "account_balance_general": formatLang(
+                            self.env, currency.round(account_sum) + 0.0, currency_obj=currency
+                        )
+                    }
                 )
-                query = """SELECT sum(%s) FROM account_move_line aml
-                            LEFT JOIN account_move move ON aml.move_id = move.id
-                            WHERE aml.account_id = %%s
-                            AND move.date <= %%s AND move.state = 'posted';""" % (amount_field,)
-                self.env.cr.execute(
-                    query,
-                    (
-                        journal.default_account_id.id,
-                        fields.Date.context_today(self),
-                    ),
-                )
-                query_results = self.env.cr.dictfetchall()
-                if query_results and query_results[0].get("sum") is not None:
-                    account_sum = query_results[0].get("sum")
-                    currency = journal.currency_id or journal.company_id.currency_id
-                    dashboard_data[journal.id].update(
-                        {
-                            "account_balance_general": formatLang(
-                                self.env, currency.round(account_sum) + 0.0, currency_obj=currency
-                            )
-                        }
-                    )
