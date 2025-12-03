8c9ff745db8e3ad9c6b657824241d4277b1b3e4d
fw-bot-adhoc <>
2025-12-03 15:58:41 +0000

    Cherry pick of e287b6e8f0ee0853fc6fa11848dba95f2ee59860 failed
    stdout:
    Auto-merging account_tax_settlement/__manifest__.py
    CONFLICT (content): Merge conflict in account_tax_settlement/__manifest__.py
    
    stderr:

--- account_accountant_ux/models/account_journal_dashboard.py
+++ account_accountant_ux/models/account_journal_dashboard.py
@@ -11,28 +11,30 @@ class AccountJournal(models.Model):
         return res
 
     def _fill_journal_dashboard_general_balance(self, dashboard_data):
-        journals = self.filtered(lambda journal: journal.type in ["bank", "cash"])
-        for journal in journals:
-            if journal.default_account_id:
-                amount_field = (
-                    "aml.balance"
-                    if (not journal.currency_id or journal.currency_id == journal.company_id.currency_id)
-                    else "aml.amount_currency"
-                )
-                query = """SELECT sum(%s) FROM account_move_line aml
-                            LEFT JOIN account_move move ON aml.move_id = move.id
-                            WHERE aml.account_id = %%s
-                            AND move.date <= %%s AND move.state = 'posted';""" % (amount_field,)
-                self.env.cr.execute(
-                    query,
-                    (
-                        journal.default_account_id.id,
-                        fields.Date.context_today(self),
-                    ),
-                )
-                query_results = self.env.cr.dictfetchall()
-                if query_results and query_results[0].get("sum") is not None:
-                    account_sum = query_results[0].get("sum")
+        journals = self.filtered(
+            lambda journal: journal.type in ("bank", "cash", "credit") and journal.default_account_id
+        )
+        if account_ids := journals.mapped("default_account_id").ids:
+            query = """SELECT account_id, sum(balance) as balance, sum(amount_currency) as amount_currency
+                            FROM account_move_line
+                            WHERE account_id in %(ids)s
+                            AND date <= %(date)s AND parent_state = 'posted'
+                            GROUP BY account_id"""
+            self.env.cr.execute(
+                query,
+                {
+                    "ids": tuple(account_ids),
+                    "date": fields.Date.context_today(self),
+                },
+            )
+            query_results = {x["account_id"]: (x["balance"], x["amount_currency"]) for x in self.env.cr.dictfetchall()}
+
+            for journal in journals:
+                if query_results and journal.default_account_id.id in query_results:
+                    if not journal.currency_id or journal.currency_id == journal.company_id.currency_id:
+                        account_sum = query_results[journal.default_account_id.id][0]
+                    else:
+                        account_sum = query_results[journal.default_account_id.id][1]
                     currency = journal.currency_id or journal.company_id.currency_id
                     dashboard_data[journal.id].update(
                         {
