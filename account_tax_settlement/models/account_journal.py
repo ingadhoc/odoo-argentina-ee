585c9e2dcff85b58c1ff6ce40906bbbf68655cc1
fw-bot-adhoc <>
2025-12-03 15:58:41 +0000

    Cherry pick of e287b6e8f0ee0853fc6fa11848dba95f2ee59860 failed
    stdout:
    Auto-merging account_tax_settlement/__manifest__.py
    CONFLICT (content): Merge conflict in account_tax_settlement/__manifest__.py
    
    stderr:

--- account_tax_settlement/models/account_journal.py
+++ account_tax_settlement/models/account_journal.py
@@ -2,7 +2,6 @@ import logging
 
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
-from odoo.tools.misc import formatLang
 from odoo.tools.safe_eval import safe_eval
 
 _logger = logging.getLogger(__name__)
@@ -333,30 +332,6 @@ class AccountJournal(models.Model):
     ###################################
     # account.journal.dashboard methods
     ###################################
-
-    def _get_journal_dashboard_data_batched(self):
-        res = super(AccountJournal, self)._get_journal_dashboard_data_batched()
-        self._fill_tax_settlement_dashboard_data(res)
-        return res
-
-    def _fill_tax_settlement_dashboard_data(self, dashboard_data):
-        """En diarios de liquidación en vista kanban agregamos al lado del botoncitos 'Líneas a liquidar' la cantidad de líneas de liquidar y el importe y al lado del botoncito 'Saldo a pagar' agregamos el importe"""
-        tax_settlement_journals = self.filtered(lambda journal: journal.tax_settlement != False)
-        if not tax_settlement_journals:
-            return
-        # TODO hacer por sql para mejorar performance
-        for journal in tax_settlement_journals:
-            currency = journal.currency_id or journal.company_id.currency_id
-            unsettled_lines = journal._get_tax_settlement_move_lines_by_tags()
-            dashboard_data[journal.id].update(
-                {
-                    "unsettled_count": len(unsettled_lines),
-                    "unsettled_amount": formatLang(
-                        self.env, -sum(unsettled_lines.mapped("balance")), currency_obj=currency
-                    ),
-                    "debit_amount": formatLang(self.env, journal.settlement_partner_id.debit, currency_obj=currency),
-                }
-            )
 
     def open_action(self):
         """
