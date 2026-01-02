e144f4031400f510f99d7790ef4a6db982b9f6bd
Juan José Scarafia <jjs@adhoc.inc>
2026-01-02 15:20:40 -0300

    [IMP] _reports: use mapped for multi recordsets
    Part-of: ingadhoc/odoo-argentina-ee#864
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- l10n_ar_account_reports/models/account_partial_reconcile.py
+++ l10n_ar_account_reports/models/account_partial_reconcile.py
@@ -7,14 +7,18 @@ class AccountPartialReconcile(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         res = super().create(vals_list)
+        returns = res.mapped("debit_move_id.move_id.closing_return_id") | res.mapped(
+            "credit_move_id.move_id.closing_return_id"
+        )
         # Disparamos actualización si alguna de las líneas pertenece a un return
-        returns = res.debit_move_id.move_id.closing_return_id | res.credit_move_id.move_id.closing_return_id
         if returns:
             returns._update_payment_state()
         return res
 
     def unlink(self):
-        returns = self.debit_move_id.move_id.closing_return_id | self.credit_move_id.move_id.closing_return_id
+        returns = self.mapped("debit_move_id.move_id.closing_return_id") | self.mapped(
+            "credit_move_id.move_id.closing_return_id"
+        )
         res = super().unlink()
         if returns:
             returns._update_payment_state()
