def5fa49338675256682f3ebee0bf85515e6b34e
Juan José Scarafia <jjs@adhoc.inc>
2026-01-13 15:50:44 -0300

    [IMP] _import_bill: better ux
    closes ingadhoc/odoo-argentina-ee#872
    
    Signed-off-by: Virginia Bonservizi <vib@adhoc.com.ar>

--- l10n_ar_import_bill/__manifest__.py
+++ l10n_ar_import_bill/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Argentinian Importing Bills from ARCA",
-    "version": "19.0.1.1.0",
+    "version": "19.0.1.2.0",
     "category": "Localization/Argentina",
     "sequence": 8,
     "author": "ADHOC SA",
