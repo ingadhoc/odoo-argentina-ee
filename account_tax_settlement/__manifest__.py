89b9303bfedc22a293c995f613c23c1ff18c75b3
Pablo Montenegro <pam@adhoc.com.ar>
2026-01-21 15:15:16 +0000

    [FIX] account_tax_settlement: vista kanban del menu "Liquidación de impuestos".
    Ticket: 107702
    En la vista kanban del menú de "Liquidación de impuestos" se ven diarios que están archivados y no queremos que eso suceda porque puede prestar a confusión.
    
    X-original-commit: e16720c6dc8768aa661a10cdf845c01ad1cef664

---
+++ account_tax_settlement/__manifest__.py
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+||||||| MERGE BASE
+=======
+{
+    "name": "Tax Settlement",
+    "version": "18.0.1.4.0",
+    "category": "Accounting",
+    "sequence": 14,
+    "summary": "",
+    "author": "ADHOC SA",
+    "website": "www.adhoc.com.ar",
+    "license": "LGPL-3",
+    "images": [],
+    "depends": [
+        # por ahora agregamos esta dep para permitir vincular a reportes
+        "account_reports",
+    ],
+    "data": [
+        "wizards/account_tax_settlement_wizard_view.xml",
+        "wizards/download_files_wizard.xml",
+        "wizards/arba_warning_template.xml",
+        "views/account_move_line_view.xml",
+        "views/account_move_view.xml",
+        "views/account_journal_view.xml",
+        "views/account_report_view.xml",
+        "security/ir.model.access.csv",
+        "data/account_report_data.xml",
+    ],
+    "demo": [],
+    "test": [],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
+
+>>>>>>> FORWARD PORTED
