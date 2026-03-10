016127c121deb8c905f98385a63b9d974c0c3914
Katherine Zaoral <kz@adhoc.inc>
2026-03-10 19:46:58 -0300

    [FIX] l10n_ar_arba_ws: Fixing name of wh lines
    Migration script to separete the cert arba number and the internal
    number between the name and ref fields. This is the behavior not only
    for new records , but also the already created lines have the same data
    and consistency

---
+++ l10n_ar_arba_ws/migrations/18.0.1.3.0/pre-migration.py
@@ -0,0 +1,21 @@
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    """Para los clientes que ya crearon retenciones quedo el en name la mezclde de "numero de cert arba + numero interno"
+    queremos que:
+    1. en el name quede solo numero cert arba
+    2. que el numero interno se guarde en el campo ref"""
+    _logger.info("Fixing wh line name: Separating ARBA cert number and internal number in name and ref fields")
+    cr.execute(r"""
+        UPDATE l10n_ar_payment_withholding
+        SET
+            ref = substring(name from '\(([^()]*)\)$'),
+            name = l10n_ar_cert_number
+        WHERE l10n_ar_cert_number IS NOT NULL
+        AND l10n_ar_cert_number <> ''
+        AND (ref IS NULL OR ref = '')
+        AND name ~ '^[^()]+ \([^()]+\)$';
+    """)
