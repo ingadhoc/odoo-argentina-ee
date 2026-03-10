5a0fd5199b190328bc78fe10aca56535c10fad67
Julia Elizondo <jue@adhoc.com.ar>
2026-03-10 18:36:54 +0000

    [ADD] l10n_ar_wsmtxca_ws: new module to support WSMTXCA webservice
    closes ingadhoc/odoo-argentina-ee#1035
    
    X-original-commit: 2fbae1e3d8b83f12de3681582d6d52faf5d4bf58
    Signed-off-by: Katherine Zaoral - kz (#l10n) <kz@adhoc.com.ar>
    Signed-off-by: Julia Elizondo - jue (#l10n) <jue@adhoc.com.ar>

---
+++ l10n_ar_wsmtxca_ws/models/account_journal.py
@@ -0,0 +1,101 @@
+from odoo import _, api, models
+from odoo.exceptions import UserError
+
+
+class AccountJournal(models.Model):
+    _inherit = "account.journal"
+
+    def _get_l10n_ar_afip_ws(self):
+        """Return the list of values of the selection field."""
+        res = super()._get_l10n_ar_afip_ws()
+        return res + [
+            ("wsmtxca", _("Codificación de producto - RG2904 (WSMTXCA)")),
+        ]
+
+    def _get_l10n_ar_afip_pos_types_selection(self):
+        """Add more options to the selection field AFIP POS System, re order options by common use"""
+        res = super()._get_l10n_ar_afip_pos_types_selection()
+        res.append(("WSMTXCAWS", _("Codificación de producto - Web Service")))
+        return res
+
+    @api.depends("l10n_ar_afip_pos_system")
+    def _compute_l10n_ar_afip_ws(self):
+        """Depending on AFIP POS System selected set the proper AFIP WS"""
+        super()._compute_l10n_ar_afip_ws()
+        for rec in self:
+            if rec.l10n_ar_afip_pos_system == "WSMTXCAWS":
+                rec.l10n_ar_afip_ws = "wsmtxca"
+
+    def l10n_ar_check_afip_pos_number(self):
+        """Return information about the AFIP POS numbers related to the given AFIP WS"""
+        if self.l10n_ar_afip_ws != "wsmtxca":
+            return super().l10n_ar_check_afip_pos_number()
+        self.ensure_one()
+        connection = self.company_id._l10n_ar_get_connection(self.l10n_ar_afip_ws)
+        client, auth = connection._get_client()
+        if self.company_id._get_environment_type() == "testing":
+            raise UserError(
+                _(
+                    '"Check Available AFIP PoS" is not implemented in testing mode for webservice %s',
+                    self.l10n_ar_afip_ws,
+                )
+            )
+        auth = self._wsmtxca_convert_auth(auth)
+        response = client.service.consultarPuntosVentaCAE(auth)
+        raise UserError(response)
+
+    def _l10n_ar_get_afip_last_invoice_number(self, document_type):
+        """Consult via webservice the number of the last invoice register"""
+        afip_ws = self.l10n_ar_afip_ws
+        if afip_ws != "wsmtxca":
+            return super()._l10n_ar_get_afip_last_invoice_number(document_type)
+        self.ensure_one()
+        if self.env.registry.in_test_mode():
+            return 0
+
+        pos_number = self.l10n_ar_afip_pos_number
+        connection = self.company_id._l10n_ar_get_connection(afip_ws)
+        client, auth = connection._get_client()
+        last = errors = False
+
+        data = self._wsmtxca_convert_auth(auth)
+        response = client.service.consultarUltimoComprobanteAutorizado(
+            data,
+            consultaUltimoComprobanteAutorizadoRequest={
+                "codigoTipoComprobante": document_type.code,
+                "numeroPuntoVenta": pos_number,
+            },
+        )
+        if response.numeroComprobante:
+            last = response.numeroComprobante
+        if response.arrayErrores:
+            errors = "".join(
+                ["\n* Code %s: %s" % (err.codigo, err.descripcion) for err in response.arrayErrores.codigoDescripcion]
+            )
+            error_first_invoice = [1502]
+            if error_first_invoice == [err.codigo for err in response.arrayErrores.codigoDescripcion]:
+                errors = False  # No invoices found for this document type
+                last = 0
+
+        if errors:
+            raise UserError(_("We received this error while consulting the last invoice number from ARCA:\n%s", errors))
+        return last
+
+    @api.model
+    def _get_codes_per_journal_type(self, afip_pos_system):
+        res = super()._get_codes_per_journal_type(afip_pos_system)
+        if afip_pos_system != "WSMTXCAWS":
+            return res
+
+        usual_codes = ["1", "2", "3", "6", "7", "8"]
+        invoice_m_code = ["51", "52", "53"]
+        mipyme_codes = ["201", "202", "203", "206", "207", "208"]
+        codes = usual_codes + invoice_m_code + mipyme_codes
+
+        res[0] = ("code", "in", codes)
+        return res
+
+    @api.model
+    def _wsmtxca_convert_auth(self, auth):
+        res = {"sign": auth.get("Sign"), "token": auth.get("Token"), "cuitRepresentada": auth.get("Cuit")}
+        return res
