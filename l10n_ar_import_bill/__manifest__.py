f9b8d13a63f1d34c730bc1ceb60f7f9a14df3654
Felipe Garcia Suez <feg@adhoc.com.ar>
2026-03-17 18:26:18 +0000

    [FIX] l10n_ar_import_bill: Update manifest for pull 938
    X-original-commit: 8bd8b988bc6ab248dde94d567d656ed7bf742b75

--- l10n_ar_import_bill/__manifest__.py
+++ l10n_ar_import_bill/__manifest__.py
@@ -1,6 +1,12 @@
 {
     "name": "Argentinian Importing Bills from ARCA",
+<<<<<<< 587ebe5f913ea55075a92c12d8e47fa36cadb003
     "version": "19.0.1.2.0",
+||||||| 3c937bcace0de216f0ecac1d2b54593b2dc70bd5
+    "version": "18.0.1.2.0",
+=======
+    "version": "18.0.1.3.0",
+>>>>>>> bb40a80a4f54a326b30aec11ad58b5022384f44e
     "category": "Localization/Argentina",
     "sequence": 8,
     "author": "ADHOC SA",
