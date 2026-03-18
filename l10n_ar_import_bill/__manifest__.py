a65cf5eccdaedfa67f591954b803cfb01eb644c9
roboadhoc <roboadhoc@adhoc.com.ar>
2026-03-18 13:02:21 +0000

    [BOT] Bump version: l10n_ar_import_bill 19.0.1.3.0
    Merged: ingadhoc/odoo-argentina-ee#945

--- l10n_ar_import_bill/__manifest__.py
+++ l10n_ar_import_bill/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Argentinian Importing Bills from ARCA",
-    "version": "19.0.1.2.0",
+    "version": "19.0.1.3.0",
     "category": "Localization/Argentina",
     "sequence": 8,
     "author": "ADHOC SA",
