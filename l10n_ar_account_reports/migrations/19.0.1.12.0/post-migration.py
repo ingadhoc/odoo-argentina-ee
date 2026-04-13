25882c6995464793348f92196dc7e36c4475017f
Katherine Zaoral <kz@adhoc.inc>
2026-04-13 20:22:21 +0000

    [FIX] l10n_ar_account_reports: migration script env
    was using old version of migrate(env, version) from opepupgradelib instead
    of new odoo style (cr, version)

--- l10n_ar_account_reports/migrations/19.0.1.12.0/post-migration.py
+++ l10n_ar_account_reports/migrations/19.0.1.12.0/post-migration.py
@@ -1,9 +1,11 @@
 import logging
 
+from odoo import SUPERUSER_ID, api
+
 _logger = logging.getLogger(__name__)
 
 
-def migrate(env, version):
+def migrate(cr, version):
     """Remove obsolete account.report.line records.
 
     The l10n_ar_pba_report_pba_withholdings_line_a122r record was an old
@@ -11,6 +13,7 @@ def migrate(env, version):
     XML file is not enough for existing databases - this migration ensures
     the record is deleted from the database to avoid orphaned references.
     """
+    env = api.Environment(cr, SUPERUSER_ID, {})
 
     # IDs of obsolete records to delete
     obsolete_line_ids = [
