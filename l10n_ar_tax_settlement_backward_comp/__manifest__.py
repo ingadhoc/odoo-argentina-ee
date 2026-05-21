e38775a0d806d3ace938553b019a5ce3337898ae
roboadhoc <roboadhoc@adhoc.com.ar>
2026-05-21 15:29:22 +0000

    [BOT] Bump version: l10n_ar_tax_settlement_backward_comp 18.0.1.1.0
    Merged: ingadhoc/odoo-argentina-ee#1028

--- l10n_ar_tax_settlement_backward_comp/__manifest__.py
+++ l10n_ar_tax_settlement_backward_comp/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Backward compatibility for tax Settlements on Argentina",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "Accounting",
     "author": "ADHOC SA",
     "website": "www.adhoc.com.ar",
