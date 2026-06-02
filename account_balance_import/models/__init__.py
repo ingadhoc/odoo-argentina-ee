6350e7388df020bb6352c1fc57f9936a958d6c11
Rocío Vega <rov@adhoc.com.ar>
2026-06-02 14:37:43 -0300

    [FIX] Account_balance_import*: Fix dependencies

--- account_balance_import/models/__init__.py
+++ account_balance_import/models/__init__.py
@@ -1,6 +1,5 @@
 from . import res_currency
 from . import account_import_summary
 from . import account_financial_year_op
-from . import account_payment
 from . import account_account
 from . import res_company
