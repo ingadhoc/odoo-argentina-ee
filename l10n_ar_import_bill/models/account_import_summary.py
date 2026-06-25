3f96d5978b2ba63516fbccaccb24af262f534b8f
Julia Elizondo <jue@adhoc.inc>
2026-06-25 14:50:14 +0000

    [IMP] l10n_ar_import_bill: make user messages translatable
    Convert all user-facing labels, fields, wizard buttons, action names
    and UserError messages to English in source, and wrap previously
    unwrapped messages in _() so they are translatable. Provide the
    Spanish translations in i18n/es.po and update the .pot template.
    ARCA Excel column identifiers and code comments are left untouched.
    
    closes ingadhoc/odoo-argentina-ee#1085
    
    Signed-off-by: Victoria Halac - hav (#l10n) <hav@adhoc.inc>

--- l10n_ar_import_bill/models/account_import_summary.py
+++ l10n_ar_import_bill/models/account_import_summary.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import _, models
 from odoo.exceptions import UserError
 
 
@@ -10,15 +10,17 @@ class AccountImportSummary(models.TransientModel):
         # Check if fiscal periods have been set (accounting_start_date must be set)
         if not self.env.company.account_opening_date:
             raise UserError(
-                "You must set fiscal periods first before importing sales from ARCA. "
-                "Please use 'Set Periods' button to configure your fiscal year."
+                _(
+                    "You must set fiscal periods first before importing sales from ARCA. "
+                    "Please use the 'Set Periods' button to configure your fiscal year."
+                )
             )
 
         return (
             self.env["afip.import.wizard"]
             .with_context(default_company_id=self.env.company.id, import_type="sale")
             ._get_records_action(
-                name="Import Sales from ARCA",
+                name=_("Import Sales from ARCA"),
                 target="new",
                 views=[(self.env.ref("l10n_ar_import_bill.view_afip_import_file_wizard_form").id, "form")],
             )
@@ -29,14 +31,16 @@ class AccountImportSummary(models.TransientModel):
         # Check if fiscal periods have been set (accounting_start_date must be set)
         if not self.env.company.account_opening_date:
             raise UserError(
-                "You must set fiscal periods first before importing purchases from ARCA. "
-                "Please use 'Set Periods' button to configure your fiscal year."
+                _(
+                    "You must set fiscal periods first before importing purchases from ARCA. "
+                    "Please use the 'Set Periods' button to configure your fiscal year."
+                )
             )
         return (
             self.env["afip.import.wizard"]
             .with_context(default_company_id=self.env.company.id, import_type="purchase")
             ._get_records_action(
-                name="Import Purchases from ARCA",
+                name=_("Import Purchases from ARCA"),
                 target="new",
                 views=[(self.env.ref("l10n_ar_import_bill.view_afip_import_file_wizard_form").id, "form")],
             )
